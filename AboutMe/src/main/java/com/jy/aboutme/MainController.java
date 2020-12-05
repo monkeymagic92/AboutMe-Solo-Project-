566278e676dd072f10e528c513e1ad5cd4f95801
project-Lee <ddw099@naver.com>
2020-12-05 19:42:26 +0900

    배포완료 / 사이트시작

---
+++ AboutMe/src/main/java/com/jy/aboutme/MainController.java
@@ -0,0 +1,17 @@
+package com.jy.aboutme;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+	@RequestMapping("/")
+	public String main(Model model) {
+		model.addAttribute("hi", "반갑다 하이");
+		return "/main/test";
+	}
+	
+}
---
+++ AboutMe/src/main/webapp/WEB-INF/views/main/test.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>ㅎㅇ 이거뜨면 성공</h1>
+	<br>
+	<h1>재용월드 온것을 환영한다 반갑다</h1>
+	<div>${hi}</div>
+</body>
+</html>
